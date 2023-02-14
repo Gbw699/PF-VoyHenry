ccc8a6ccf4eeb1367aeef9697a1abb35f346dc40
MatiasxVarela <saxtebg@gmail.com>
2023-02-14 11:05:55 -0300

    Create get all users route

--- api/src/routes/users.routes.js
+++ api/src/routes/users.routes.js
@@ -1,1 +1,21 @@
-/* lien crea a los usuarios */
+const { Router } = require('express');
+
+const router = Router();
+
+/* Get all users */
+
+router.get('/', (req, res, next) => {
+
+  try {
+
+    res.json({message: "Esta ruta debería mostrar todos los usuarios"})
+  } catch (error) {
+
+    next(error)
+  }
+
+});
+
+/*  */
+
+module.exports = router;
