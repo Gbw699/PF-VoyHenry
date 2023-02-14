2f439bc193996598c791defc94fdb392a9531e48
Julian <cococastro619@gmail.com>
2023-02-14 12:00:03 -0300

    Creación del Home y PlansComponent

--- client/src/views/Home/Home.jsx
+++ client/src/views/Home/Home.jsx
@@ -3,7 +3,15 @@ import React from "react"
 export default function Home () {
     return (
         <>
-            
+            {/* 
+                NAVBAR
+                PROFILE INFO
+                PLANS
+                SECTIONS (FILTERED-PLANS)
+                BLOG REVIEW
+                FOOTER
+            */}
+            {/* <Plans/> */}
         </>
     )
 }
