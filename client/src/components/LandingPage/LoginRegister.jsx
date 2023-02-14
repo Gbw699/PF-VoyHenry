7c7683d8a74ff3ec67e6daef387e9115506f0b80
Marcos Hernan Parella <marcosparella2000@gmail.com>
2023-02-14 12:50:21 -0300

    Se siguió un poco más el LoginRegister

--- client/src/components/LandingPage/LoginRegister.jsx
+++ client/src/components/LandingPage/LoginRegister.jsx
@@ -1,3 +1,27 @@
+import { Button } from "@mui/material";
+
 export default function LoginRegister() {
-  return <Button variant="contained">Contained</Button>;
+  const handleSubmit = (value) => {
+    console.log(value.target.value);
+  };
+  return (
+    <>
+      <Button
+        type="submit"
+        variant="contained"
+        value="logIn"
+        onClick={(event) => handleSubmit(event)}
+      >
+        Iniciar sesión
+      </Button>
+      <Button
+        type="submit"
+        variant="contained"
+        value="singUp"
+        onClick={(event) => handleSubmit(event)}
+      >
+        Registrarse
+      </Button>
+    </>
+  );
 }
