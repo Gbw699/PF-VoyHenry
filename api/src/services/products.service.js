218d363102135cc1cd937830b9981551bacc3502
MatiasxVarela <saxtebg@gmail.com>
2023-02-15 18:13:40 -0300

    Create service products template

---
+++ api/src/services/products.service.js
@@ -0,0 +1,37 @@
+class ProductsService {
+
+  constructor(){
+
+  }
+
+  /* create product */
+
+  create () {
+
+  }
+
+  /* find all products */
+
+  find () {
+
+  }
+
+  /* find one product */
+
+  findOne () {
+
+  }
+
+  update () {
+
+  }
+
+  /* delete one product */
+
+  delete () {
+
+  }
+
+}
+
+module.exports = ProductsService;
