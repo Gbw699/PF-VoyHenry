ed132b1ea926ad5127d43a4d0fc96633a5011b10
Gabriel Barimboim <gdbarimboim@gmail.com>
2023-02-15 13:22:58 -0300

    account menu ready

--- client/src/components/Navbar/AccountMenu.jsx
+++ client/src/components/Navbar/AccountMenu.jsx
@@ -1,7 +1,87 @@
+import * as React from "react";
+import Avatar from "@mui/material/Avatar";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import Divider from "@mui/material/Divider";
+import IconButton from "@mui/material/IconButton";
+import Tooltip from "@mui/material/Tooltip";
+//remplazar la imagen por la del usuario
+import img from "../../assets/voyHENRY_logo.png";
+
 export default function AccountMenu() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const open = Boolean(anchorEl);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
-    <div>
-      <h2>menu del perfil</h2>
-    </div>
+    <React.Fragment>
+      <Tooltip title="Account settings">
+        <IconButton
+          onClick={handleClick}
+          size="small"
+          aria-controls={open ? "account-menu" : undefined}
+          aria-haspopup="true"
+          aria-expanded={open ? "true" : undefined}
+        >
+          <Avatar
+            srcSet={img}
+            sx={{ width: 32, height: 32 }}
+          >
+            I
+          </Avatar>
+        </IconButton>
+      </Tooltip>
+      <Menu
+        anchorEl={anchorEl}
+        id="account-menu"
+        open={open}
+        onClose={handleClose}
+        onClick={handleClose}
+        PaperProps={{
+          elevation: 0,
+          sx: {
+            overflow: "visible",
+            filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
+            mt: 0.5,
+            "& .MuiAvatar-root": {
+              width: 32,
+              height: 32,
+              ml: -0.5,
+              mr: 1,
+            },
+            "& .MuiButtonBase-root": {
+              pt: 0.3,
+              pb: 0.3,
+            },
+          },
+        }}
+        transformOrigin={{ horizontal: "right", vertical: "top" }}
+        anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
+      >
+        <MenuItem
+          onClick={handleClose}
+          sx={{ justifyContent: "center" }}
+        >
+          Nombre del usuario
+        </MenuItem>
+        <Divider />
+        <MenuItem onClick={handleClose}>Carrito</MenuItem>
+        <Divider />
+        <MenuItem onClick={handleClose}>Mi perfil</MenuItem>
+        <MenuItem onClick={handleClose}>Mis planes</MenuItem>
+        <Divider />
+        <MenuItem onClick={handleClose}>Editar perfil</MenuItem>
+        <MenuItem onClick={handleClose}>Configuración</MenuItem>
+        <Divider />
+        <MenuItem onClick={handleClose}>Cerrar sesión</MenuItem>
+      </Menu>
+    </React.Fragment>
   );
 }
