3ae810fce12633018500690da7fc7bff2c85c272
Marcos Hernan Parella <marcosparella2000@gmail.com>
2023-02-15 17:39:54 -0300

    Nuevas cosas

--- client/src/components/Home/HomePlansSection.jsx
+++ client/src/components/Home/HomePlansSection.jsx
@@ -3,10 +3,10 @@ import PlanSection from "./PlanSection";
 export default function HomePlansSections() {
   return (
     <>
-      <PlanSection title="Próximos eventos"/>
-      <PlanSection title="Eventos en proceso"/>
-      <PlanSection title="Eventos finalizados"/>
-      <PlanSection title="Eventos on-line"/>
+      <PlanSection title="Próximos eventos" />
+      <PlanSection title="Eventos en proceso" />
+      <PlanSection title="Eventos finalizados" />
+      <PlanSection title="Eventos on-line" />
     </>
   );
 }
