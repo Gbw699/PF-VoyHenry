fdfc9470d80cb462c96f7a57988ddd4ab8306f45
MatiasxVarela <saxtebg@gmail.com>
2023-02-15 16:01:38 -0300

    add grater and less date user  Birth validation

--- api/src/schemas/users.schema.js
+++ api/src/schemas/users.schema.js
@@ -1,10 +1,18 @@
 const Joi = require('joi')
 
+/* This is to see the current date and approximately 99 years ago and 18 years ago or so. */
+
+const currentDate = new Date();
+const greaterDate = `${currentDate.getFullYear() - 99}-${currentDate.getMonth() + 1}-${currentDate.getDate()}`;
+const lessDate = `${currentDate.getFullYear() - 18}-${currentDate.getMonth() + 1}-${currentDate.getDate()}`;
+
+//
+
 const nickName = Joi.string().alphanum().min(3).max(15)
 const email = Joi.string().email()
 const genre = Joi.string().valid('Femenino', 'Masculino', 'No binario', 'Prefiero no decirlo', 'Otro');
 const about = Joi.string().max(255)
-const dateOfBirth = Joi.date()
+const dateOfBirth = Joi.date().greater(greaterDate).less(lessDate);
 const firstName = Joi.string().alphanum().min(3).max(55)
 const lastName = Joi.string().alphanum().min(3).max(55)
 const image = Joi.string().uri()
