5faa2fde9c9203c516d681108d8b74a6a3db4fa5
MatiasxVarela <saxtebg@gmail.com>
2023-02-16 18:52:45 -0300

    add auth local strategy

--- api/src/utils/auth/strategies/local.strategy.js
+++ api/src/utils/auth/strategies/local.strategy.js
@@ -1,9 +1,25 @@
+const { CustomError } = require('../../../middlewares/error.handler')
 const { Strategy } = require('passport-local');
+const bcrypt = require('bcrypt')
 const UsersService = require('../../../services/user.service')
 
 const service =  new UsersService();
 
-const LocalStrategy = new Strategy((email, password, done) => {
+const LocalStrategy = new Strategy( async (email, password, done) => {
+
+  try {
+
+    const user = await service.findByEmail(email)
+
+    const isMatch = await bcrypt.compare(password, user.password)
+
+    if (!isMatch) {
+      done(new CustomError("unauthorized", 401), false)
+    }
+    done(null, user)
+  } catch (error) {
+    done(error, false)
+  }
 
 });
 
