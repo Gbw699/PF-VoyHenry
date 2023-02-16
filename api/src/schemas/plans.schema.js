5f9684ea77235080123bf3c088084a63ea4d79f0
liensanchez <liensilviosanchez@gmail.com>
2023-02-15 22:23:09 -0300

    images are not required

--- api/src/schemas/plans.schema.js
+++ api/src/schemas/plans.schema.js
@@ -9,7 +9,6 @@ const title = Joi.string().min(3).max(15)
 const summary = Joi.string().max(125)
 const description = Joi.string().min(3).max(255)
 const mainImage = Joi.string().uri()
-const images = Joi.string().array()
 const state = Joi.string().valid('En Planeacion', 'En progreso',  'Finalizado')
 const eventDate = Joi.date().greater(greaterDate);
 
@@ -22,7 +21,6 @@ const createPlanSchema = Joi.object({
   summary: summary.required(),
   description: description.required(),
   mainImage: mainImage.required(),
-  images: images.required(),
   state: state.required(),
   eventDate: eventDate.required()
 })
