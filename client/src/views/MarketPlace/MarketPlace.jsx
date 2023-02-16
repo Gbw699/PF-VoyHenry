6b2b832dc90ff33d59ac571939af8d81e9714ce3
Marcos Hernan Parella <marcosparella2000@gmail.com>
2023-02-16 16:30:57 -0300

    Se arregló FormLogIn y app

--- client/src/views/MarketPlace/MarketPlace.jsx
+++ client/src/views/MarketPlace/MarketPlace.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-// import style from "./MarketPlace.module.css";
 import MarketPlaceSection from "../../components/MarketPlace/MarketPlaceSection";
+// import style from "./MarketPlace.module.css";
 export default function MarketPlace() {
   return (
     <div>
