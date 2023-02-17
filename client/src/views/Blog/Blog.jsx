ca2dec5ec2a66074d0382ac15b732ccf1e78aeef
Gabriel Barimboim <gdbarimboim@gmail.com>
2023-02-17 19:19:31 -0300

    blogForm created

--- client/src/views/Blog/Blog.jsx
+++ client/src/views/Blog/Blog.jsx
@@ -1,14 +1,27 @@
+import { useState } from "react";
 import ProfileCard from "../../recycle/ProfileCard/ProfileCard";
 import BlogsReviews from "../../components/Blog/BlogsReviews";
+import BlogForm from "../../recycle/BlogForm/BlogForm";
 import BlogUsers from "../../components/Blog/BlogUsers";
 import style from "./Blog.module.css";
 
 export default function Blog() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div className={style.container}>
       <div className={style.profileCont}>
         <ProfileCard />
-        <button type="submit" className={style.writeBtn}>Escribir reseña</button>
+        <button
+          onClick={() => setIsOpen(true)}
+          className={style.writeBtn}
+        >
+          Escribir reseña
+        </button>
+        <BlogForm
+          open={isOpen}
+          close={() => setIsOpen(false)}
+        />
       </div>
       <BlogsReviews />
       <BlogUsers />
