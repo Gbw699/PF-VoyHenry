3530eb7745db019a6716c85306ccb1a7075cd106
FacuTombesi <facundomtombesi@gmail.com>
2023-02-17 11:27:00 -0300

    CSS Footer

--- client/src/views/Footer/Footer.jsx
+++ client/src/views/Footer/Footer.jsx
@@ -1,12 +1,13 @@
 import React from "react";
-// import style from "./Footer.module.css";
+import style from "./Footer.module.css";
 import FooterImg from "../../components/Footer/FooterImg";
 import FooterText from "../../components/Footer/FooterText";
+
 export default function Footer() {
   return (
-    <>
+    <div className={style.footerCont}>
       <FooterImg />
       <FooterText />
-    </>
+    </div>
   );
 }
