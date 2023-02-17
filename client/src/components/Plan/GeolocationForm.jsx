fa52da4e45bac610728cdfc611ef2157af8479f5
Julian <cococastro619@gmail.com>
2023-02-17 17:22:38 -0300

    Arreglos en Plan

--- client/src/components/Plan/GeolocationForm.jsx
+++ client/src/components/Plan/GeolocationForm.jsx
@@ -3,7 +3,15 @@ import React from "react";
 export default function GeolocationForm() {
   return (
     <div>
-      Geolocation
+      <h3>Filtros</h3>
+      <div>
+        <h3>Fecha</h3>
+        <input type="text" />
+        <h3>Lugar</h3>
+        <input type="text" />
+        <h3>Valoración</h3>
+        <input type="text" />
+      </div>
     </div>
   );
 }
