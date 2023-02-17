9680e15c20bf5b243f0b82be6e56d4586fba9484
Julian <cococastro619@gmail.com>
2023-02-17 14:57:20 -0300

    Pagination

--- client/src/recycle/PlanCardList/PlansCardList.jsx
+++ client/src/recycle/PlanCardList/PlansCardList.jsx
@@ -24,6 +24,7 @@ export default function PlanCardList() {
                 count={Math.ceil(plansData.data.length / plansPerPage)}
                 page={page}
                 onChange={handlePageChange}
+                
             />
 
             <div className={style.cardCont}>
