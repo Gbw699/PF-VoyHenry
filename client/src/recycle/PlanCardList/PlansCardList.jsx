94335a2633a5ccc7ab366c507f51da263e195974
Julian <cococastro619@gmail.com>
2023-02-17 14:56:37 -0300

    Pagination

--- client/src/recycle/PlanCardList/PlansCardList.jsx
+++ client/src/recycle/PlanCardList/PlansCardList.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import plansData from "../../plans.json";
 import PlanCard from "../PlanCard/PlanCard";
 import style from "./PlansCardList.module.css";
-import { Card, Pagination } from "@mui/material";
+import { Pagination } from "@mui/material";
 
 export default function PlanCardList() {
     const [page, setPage] = useState(1);
@@ -13,42 +13,28 @@ export default function PlanCardList() {
         setPage(value);
     };
 
-export default function Plan() {
-  const plans = plansData.data;
-  return (
-    <div>
-      <h3>Planes Destacados</h3>
-      <hr
-        width="100%"
-        color="#F1E100"
-      />
-
-      <div className={style.cardCont}>
-        {plans.map((plan) => (
-          <PlanCard
-            key={plan.id}
-            src={plan.mainImage}
-            title={plan.title}
-          />
-        ))}
-      </div>
-    </div>
-  );
     return (
-        <Card>
+        <div>
+            <h3>Planes Destacados</h3>
+            <hr
+                width="100%"
+                color="#F1E100"
+            />
             <Pagination
                 count={Math.ceil(plansData.data.length / plansPerPage)}
                 page={page}
                 onChange={handlePageChange}
             />
-            {plans.map((plan) => (
-                <PlanCard key={plan.id}
-                    mainImage={plan.mainImage}
-                    title={plan.title}
-                />
-            ))}
-        </Card>
+
+            <div className={style.cardCont}>
+                {plans.map((plan) => (
+                    <PlanCard
+                        key={plan.id}
+                        src={plan.mainImage}
+                        title={plan.title}
+                    />
+                ))}
+            </div>
+        </div>
     );
 }
-
-
