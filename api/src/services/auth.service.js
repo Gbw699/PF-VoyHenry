7c5b94218a69b9fa878edddbe99770eb06d6288b
MatiasxVarela <saxtebg@gmail.com>
2023-02-18 18:27:49 -0300

    Add auth get user service

--- api/src/services/auth.service.js
+++ api/src/services/auth.service.js
@@ -1,9 +1,29 @@
+const UsersService = require('./user.service')
+const { CustomError } = require('../middlewares/error.handler')
+const bcrypt = require('bcrypt')
+
+const userService = new UsersService()
 
 class AuthService {
 
-  getUser(email, password) {
+  async getUser(email, password) {
+
+    const user = await userService.findByEmail(email)
+
+    console.log(user)
+
+    const isMatch = await bcrypt.compare(password, user.password)
+
+    if (!isMatch) {
+      throw new CustomError("unauthorized", 401)
+    }
+
+    delete user.dataValues.password;
+
+    return user
 
   }
+
 
 }
 
--- api/src/utils/auth/strategies/local.strategy.js
+++ api/src/utils/auth/strategies/local.strategy.js
@@ -1,9 +1,7 @@
-const { CustomError } = require('../../../middlewares/error.handler')
 const { Strategy } = require('passport-local');
-const bcrypt = require('bcrypt')
-const UsersService = require('../../../services/user.service')
+const AuthService = require('../../../services/auth.service')
 
-const service =  new UsersService();
+const service =  new AuthService();
 
 const LocalStrategy = new Strategy({
   usernameField: 'email',
@@ -12,15 +10,7 @@ const LocalStrategy = new Strategy({
   async (email, password, done) => {
     try {
 
-      const user = await service.findByEmail(email)
-
-      const isMatch = await bcrypt.compare(password, user.password)
-
-      if (!isMatch) {
-        done(new CustomError("unauthorized", 401), false)
-      }
-
-      delete user.dataValues.password;
+      const user = await service.getUser(email, password)
 
       done(null, user)
     } catch (error) {
