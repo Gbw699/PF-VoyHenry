f6e241c5653b580ef8ccc7e508f310763bab8baf
FacuTombesi <facundomtombesi@gmail.com>
2023-02-18 16:53:46 -0300

    CSS Plan actualizado

--- client/src/components/Plan/GeolocationForm.jsx
+++ client/src/components/Plan/GeolocationForm.jsx
@@ -1,16 +1,18 @@
 import React from "react";
+import style from "./GeolocationForm.module.css";
 
 export default function GeolocationForm() {
   return (
-    <div>
-      <h3>Filtros</h3>
-      <div>
-        <h3>Fecha</h3>
-        <input type="text" />
-        <h3>Lugar</h3>
-        <input type="text" />
-        <h3>Valoración</h3>
-        <input type="text" />
+    <div className={style.container}>
+      <h3 className={style.title}>Filtros</h3>
+      <hr color="#F1E100" width="100%" />
+      <div className={style.filtersCont}>
+        <h3 className={style.filterTitle}>Fecha</h3>
+        <input type="text" placeholder="Seleccionar fecha" className={style.inputs} />
+        <h3 className={style.filterTitle}>Lugar</h3>
+        <input type="text" placeholder="Seleccionar locación" className={style.inputs} />
+        <h3 className={style.filterTitle}>Valoración</h3>
+        <input type="text" placeholder="Filtrar por valoraciones" className={style.inputs} />
       </div>
     </div>
   );
