314d4a54f8f5f8b49333e1b9a7451c4cf48ddc63
Gabriel Barimboim <gdbarimboim@gmail.com>
2023-02-20 18:41:08 -0300

    blog section fixed

--- client/src/recycle/BlogForm/BlogForm.jsx
+++ client/src/recycle/BlogForm/BlogForm.jsx
@@ -1,7 +1,7 @@
 import { ErrorMessage, Field, Form, Formik } from "formik";
 import { useDispatch } from "react-redux";
 import * as Yup from "yup";
-import { postBlog } from "../../redux/slices/blogSlice/thunk";
+import { postBlog, getBlogs } from "../../redux/slices/blogSlice/thunk";
 import style from "./BlogForm.module.css";
 
 export default function BlogForm({ open, close }) {
@@ -14,10 +14,13 @@ export default function BlogForm({ open, close }) {
     <div className={style.container}>
       <div className={style.writeCont}>
         <h2 className={style.title}>Escribe tu reseña</h2>
-        <hr color="#F1E100" width="100%" />
+        <hr
+          color="#F1E100"
+          width="100%"
+        />
         <Formik
           initialValues={{
-            usernickName: nickname,
+            userNickName: nickname,
             title: "",
             content: "",
             rating: 0,
@@ -26,11 +29,15 @@ export default function BlogForm({ open, close }) {
           validationSchema={Yup.object({
             title: Yup.string()
               .min(3, "Debe tener más de 3 caracteres")
-              .max(30, "Debe tener menos de 30 caracteres"),
-            content: Yup.string().max(500, "Debe tener menos de 500 caracteres"),
+              .max(30, "Debe tener menos de 30 caracteres")
+              .required("El título es obligatorio"),
+            content: Yup.string()
+              .max(500, "Debe tener menos de 500 caracteres")
+              .required("El contenido de la reseña es obligatoria"),
             rating: Yup.number()
               .min(0, "Debe ser mayor o igual que 0")
-              .max(5, "Debe ser menor o igual que 5"),
+              .max(5, "Debe ser menor o igual que 5")
+              .required("La valoración es obligatoria"),
             image: Yup.string().url(),
           })}
           onSubmit={(values) => {
@@ -38,38 +45,68 @@ export default function BlogForm({ open, close }) {
           }}
         >
           <Form className={style.formCont}>
-            <label htmlFor="title" className={style.formLabel}>Título</label>
+            <label
+              htmlFor="title"
+              className={style.formLabel}
+            >
+              Título
+            </label>
             <Field
               name="title"
               type="text"
             />
             <ErrorMessage name="title" />
 
-            <label htmlFor="content" className={style.formLabel}>Reseña</label>
+            <label
+              htmlFor="content"
+              className={style.formLabel}
+            >
+              Reseña
+            </label>
             <Field
               name="content"
               as="textarea"
             />
             <ErrorMessage name="content" />
 
-            <label htmlFor="rating" className={style.formLabel}>Valoración</label>
+            <label
+              htmlFor="rating"
+              className={style.formLabel}
+            >
+              Valoración
+            </label>
             <Field
               name="rating"
               type="number"
             />
             <ErrorMessage name="rating" />
 
-            <label htmlFor="image" className={style.formLabel}>Imagen de carátula</label>
+            <label
+              htmlFor="image"
+              className={style.formLabel}
+            >
+              Imagen de carátula
+            </label>
             <Field
               name="image"
               type="url"
             />
             <ErrorMessage name="image" />
 
-            <button type="submit" className={style.submitBtn}>Subir reseña</button>
+            <button
+              type="submit"
+              className={style.submitBtn}
+            >
+              Subir reseña
+            </button>
           </Form>
         </Formik>
-        <button onClick={close} className={style.closeBtn}>Volver</button>
+        <button
+          onClick={close}
+          className={style.closeBtn}
+        >
+          Volver
+        </button>
       </div>
     </div>
   );
