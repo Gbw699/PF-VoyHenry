8784c317e64f4583189ea20de5cad583c71e40c0
Marcos Hernan Parella <marcosparella2000@gmail.com>
2023-02-20 20:21:00 -0300

    Profile casi terminado

--- client/src/components/Profile/ProfileSection.jsx
+++ client/src/components/Profile/ProfileSection.jsx
@@ -9,30 +9,36 @@ import ProfileMyPlans from "./ProfileMyPlans";
 import profileData from "../../profileData.json";
 import style from "./ProfileSection.module.css";
 import { useDispatch, useSelector } from "react-redux";
-import { getUser } from "../../redux/slices/userSlice/thunks";
+import {
+  getUser,
+  getUserPlans,
+  getUserBlogs,
+} from "../../redux/slices/userSlice/thunks";
 
 export default function ProfileSection() {
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(getUser("Leslie"));
-  }, [dispatch]);
+    dispatch(getUserPlans("Leslie"));
+    dispatch(getUserBlogs("Leslie"));
+  }, []);
 
-  // const user = useSelector((state) => state.userStore.user.data.user);
-
-  // console.log(user);
+  const user = useSelector((state) => state.userStore);
 
   return (
     <div className={style.mainContainer}>
       <div className={style.profileCont}>
-        {profileData.data.map((element) => (
-          <div key={element.id}>
+        {user.user.map((element) => (
+          <div key={element.nickName}>
             <ProfileInfo
               image={element.image}
-              name={element.name}
-              nationality={element.nationality}
-              following={element.following}
-              followers={element.followers}
-              assistedPlans={element.assistedPlans}
+              firstName={element.firstName}
+              lastName={element.lastName}
+              genre={element.genre}
+              nationality="Argentina"
+              following="156"
+              followers="165"
+              assistedPlans="12"
               plansCreated={element.plansCreated}
               reviewsCreated={element.reviewsCreated}
             />
@@ -67,10 +73,10 @@ export default function ProfileSection() {
               color="#F1E100"
               width="100%"
             />
-            {profileData.data.map((element, index) => (
+            {user.user.map((element, index) => (
               <ProfileAboutMe
                 key={index}
-                aboutMe={element.profileAboutMe}
+                aboutMe={element.about}
               />
             ))}
           </div>
@@ -81,20 +87,18 @@ export default function ProfileSection() {
               width="100%"
             />
             <div className={style.plansCont}>
-              {profileData.data.map((element) =>
-                element.latestAssistedPlans.map((element2) => (
-                  <Link
-                    key={element2.id}
-                    to={`/plans/${element2.id}`}
-                    className={style.link}
-                  >
-                    <ProfileLatestAssistedPlans
-                      latestAssistedPlansImg={element2.latestAssistedPlansImg}
-                      latestAssistedPlansName={element2.latestAssistedPlansName}
-                    />
-                  </Link>
-                ))
-              )}
+              {profileData.data.map((element) => (
+                <Link
+                  key={element.id}
+                  to={`/plans/${element.id}`}
+                  className={style.link}
+                >
+                  <ProfileLatestAssistedPlans
+                    latestAssistedPlansImg={element.latestAssistedPlansImg}
+                    latestAssistedPlansName={element.latestAssistedPlansName}
+                  />
+                </Link>
+              ))}
             </div>
           </div>
           <div>
@@ -104,20 +108,18 @@ export default function ProfileSection() {
               width="100%"
             />
             <div className={style.plansCont}>
-              {profileData.data.map((element) =>
-                element.latestAssistedPlans.map((element2) => (
-                  <Link
-                    key={element2.id}
-                    to={`/plans/${element2.id}`}
-                    className={style.link}
-                  >
-                    <ProfileMyPlans
-                      myPlansImage={element2.latestAssistedPlansImg}
-                      myPlansName={element2.latestAssistedPlansName}
-                    />
-                  </Link>
-                ))
-              )}
+              {user.userPlans.map((element) => (
+                <Link
+                  key={element.id}
+                  to={`/plans/${element.id}`}
+                  className={style.link}
+                >
+                  <ProfileMyPlans
+                    myPlansImage={element.mainImage}
+                    myPlansName={element.title}
+                  />
+                </Link>
+              ))}
             </div>
           </div>
           <div>
@@ -127,23 +129,19 @@ export default function ProfileSection() {
               width="100%"
             />
             <div className={style.plansCont}>
-              {profileData.data.map((element) =>
-                element.latestReview.map((element2) => (
-                  <Link
-                    key={element2.id}
-                    to={`/plans/${element2.id}`}
-                  >
-                    <ProfileLatestReviews
-                      latestReviewsImg={element2.latestReviewsImg}
-                      latestReviewsName={element2.latestReviewsName}
-                      latestReviewsDescription={
-                        element2.latestReviewsDescription
-                      }
-                      latestReviewsAssessment={element2.latestReviewsAssessment}
-                    />
-                  </Link>
-                ))
-              )}
+              {user.userBlogs.map((element) => (
+                <Link
+                  key={element.id}
+                  to={`/blog/${element.id}`}
+                >
+                  <ProfileLatestReviews
+                    image={element.image}
+                    name={element.title}
+                    description={element.content}
+                    assessment="50"
+                  />
+                </Link>
+              ))}
             </div>
           </div>
         </div>
