d8632c4ccc2c5d8e4cd5e155d3fa35fed8e8d8ec
Marcos Hernan Parella <marcosparella2000@gmail.com>
2023-02-20 20:49:42 -0300

    profile terminado

--- client/src/components/Profile/ProfileMyFriendsActivity.jsx
+++ client/src/components/Profile/ProfileMyFriendsActivity.jsx
@@ -2,23 +2,23 @@ import React from "react";
 import { useEffect, useState } from "react";
 import style from "./ProfileMyFriendsActivity.module.css";
 
-export const ProfileMyFriendsActivity = ({
-  myFriendsActivityImg,
-  myFriendsActivityName,
-}) => {
+export const ProfileMyFriendsActivity = ({ image, name }) => {
   const [backgroundImage, setBackgroundImage] = useState("");
 
   useEffect(() => {
-    setBackgroundImage(`url(${myFriendsActivityImg})`);
+    setBackgroundImage(`url(${image})`);
   });
 
   return (
     <div className={style.container}>
       <div>
-        <div className={style.img} style={{ backgroundImage: backgroundImage }} />
+        <div
+          className={style.img}
+          style={{ backgroundImage: backgroundImage }}
+        />
       </div>
       <div>
-        <p className={style.title}>{myFriendsActivityName}</p>
+        <p className={style.title}>{name}</p>
       </div>
     </div>
   );
