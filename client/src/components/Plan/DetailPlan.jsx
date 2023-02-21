53ffca4ec991bbda04c8efaeca57040df62ab8cc
ThomasLiendo <thomasliendo00@gmail.com>
2023-02-20 23:45:54 -0300

    nada

--- client/src/components/Plan/DetailPlan.jsx
+++ client/src/components/Plan/DetailPlan.jsx
@@ -1,26 +1,32 @@
 import React from "react";
 
 export default function DetailPlan() {
-
-    return (
-        <div>
-            <div style={{backgroundImage: "url(https://loremflickr.com/640/480/abstract)"}} >
-                <h1>Titulo del Plan</h1>
-                <h3>Localidad</h3>
-                <h3>Fecha</h3>
-            </div>
-            <div>
-                <h1>Creador</h1>
-                <p>Título Descripción</p>
-                <hr />
-                <p>Descripción</p>
-            </div>
-            <div>
-                <h1>Galería</h1>
-                <hr />
-                <hr />
-                <img src="https://loremflickr.com/640/480/abstract" alt="img" />
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <div
+        style={{
+          backgroundImage: "url(https://loremflickr.com/640/480/abstract)",
+        }}
+      >
+        <h1>Titulo del Plan</h1>
+        <h3>Localidad</h3>
+        <h3>Fecha</h3>
+      </div>
+      <div>
+        <h1>Creador</h1>
+        <p>Título Descripción</p>
+        <hr />
+        <p>Descripción</p>
+      </div>
+      <div>
+        <h1>Galería</h1>
+        <hr />
+        <hr />
+        <img
+          src="https://loremflickr.com/640/480/abstract"
+          alt="img"
+        />
+      </div>
+    </div>
+  );
 }
