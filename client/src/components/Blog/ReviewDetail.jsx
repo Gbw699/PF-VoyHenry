af61f4614f57ecdd93e80feaa0d00e87aefb81c2
Gabriel Barimboim <gdbarimboim@gmail.com>
2023-02-21 18:49:15 -0300

    review blogs detail created

--- client/src/components/Blog/ReviewDetail.jsx
+++ client/src/components/Blog/ReviewDetail.jsx
@@ -1,8 +1,20 @@
 export default function ReviewDetail({ blog }) {
-    console.log(blog);
+  console.log(blog);
   return (
     <div>
-      <img src={blog.userImage} alt="img" />
+      <img
+        src={blog.userimage}
+        alt="img"
+      />
+      <h3>{blog.userNickName}</h3>
+      <p>{blog.createdAt.slice(0, 10)}</p>
+      <img
+        src={blog.image}
+        alt=""
+      />
+      <h4>{blog.title}</h4>
+      <p>{blog.content}</p>
+      <p>{blog.rating}</p>
     </div>
   );
 }
