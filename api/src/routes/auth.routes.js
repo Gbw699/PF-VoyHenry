ba4fa3ad27cec60c1302facd03948f5e31c4da7b
MatiasxVarela <saxtebg@gmail.com>
2023-02-24 11:48:53 -0300

    add jwt in google route call back

--- api/src/routes/auth.routes.js
+++ api/src/routes/auth.routes.js
@@ -29,18 +29,29 @@ router.post('/login',
 /* login with google */
 
 router.get('/login/google',
-  passport.authenticate('google', { session: false, scope: ['profile', 'email', 'https://www.googleapis.com/auth/user.birthday.read', 'https://www.googleapis.com/auth/user.gender.read']})
+  passport.authenticate('google', { session: false, scope: [
+    'profile',
+    'email',
+    'https://www.googleapis.com/auth/user.birthday.read',
+    'https://www.googleapis.com/auth/user.gender.read'
+  ]})
 );
 
 /* login with google callback */
 
 router.get('/login/google/callback',
-  passport.authenticate('google', { session: false, scope: ['profile', 'email', 'https://www.googleapis.com/auth/user.birthday.read', 'https://www.googleapis.com/auth/user.gender.read']}),
+  passport.authenticate('google', { session: false, scope: [
+    'profile',
+    'email',
+    'https://www.googleapis.com/auth/user.birthday.read',
+    'https://www.googleapis.com/auth/user.gender.read'
+  ]}),
   async (req, res, next) => {
 
     try {
+      const user = req.user;
 
-      res.json(req.user)
+      res.json(service.signToken(user))
     } catch (error) {
 
       next(error)
