2fa4c4ddf8cf299e6e75d8b8b27ea0940f6158bb
Julian <cococastro619@gmail.com>
2023-02-25 13:30:02 -0300

    ajustes varios

--- client/src/recycle/FilteredPlans/queryStringPlan.js
+++ client/src/recycle/FilteredPlans/queryStringPlan.js
@@ -44,5 +44,5 @@ export const queryString = (filter, order) => {
   }
 
   const queryParams = Object.values(params).join("");
-  return queryParams ? `?${queryParams.substr(1)}` : "";
+  return queryParams ? `&${queryParams}` : "";
 };
