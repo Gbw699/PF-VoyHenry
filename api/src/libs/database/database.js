952b9e502d10bb532579b3acbc107bed62802b5f
MatiasxVarela <saxtebg@gmail.com>
2023-02-26 13:29:13 -0300

    Deploy by fly

--- api/src/libs/database/database.js
+++ api/src/libs/database/database.js
@@ -2,15 +2,9 @@ require('dotenv').config();
 const Sequelize = require('sequelize')
 
 const {
-  DB_USER,
-  DB_PASSWORD,
-  DB_NAME,
-  DB_HOST
+  DB_URL
 } = process.env
 
-const sequelize = new Sequelize ( DB_NAME, DB_USER, DB_PASSWORD, {
-  host: DB_HOST,
-  dialect: 'postgres'
-})
+const sequelize = new Sequelize(DB_URL)
 
 module.exports = sequelize;
