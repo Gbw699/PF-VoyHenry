e2076616dc6f089eb0a14ca1379d4e627ab438da
Marcos Hernan Parella <marcosparella2000@gmail.com>
2023-02-27 15:12:27 -0300

    Nuevo cambios

--- client/src/components/EditProfile/EditProfileState.jsx
+++ client/src/components/EditProfile/EditProfileState.jsx
@@ -6,7 +6,7 @@ export default function EditProfileState() {
   const user = JSON.parse(localStorage.getItem("user"));
   const [firstName, setFirstName] = useState(user.firstName);
   const [lastName, setLastName] = useState(user.lastName);
-  const [nickName, setNickName] = useState(user.firstName);
+  const [nickName, setNickName] = useState(user.nickName);
   const [about, setAbout] = useState(user.about);
   const [image, setImage] = useState(user.image);
   const [provinces, setProvinces] = useState([]);
