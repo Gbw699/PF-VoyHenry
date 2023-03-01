00f8f12a0940d0783cc24ecef2381cceae81b148
FacuTombesi <facundomtombesi@gmail.com>
2023-03-01 16:17:33 -0300

    CSS fix 2

--- client/src/views/MarketPlace/MarketPlace.jsx
+++ client/src/views/MarketPlace/MarketPlace.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import MarketPlaceSection from "../../components/MarketPlace/MarketPlaceSection";
-import style from "./MarketPlace.module.css";
+// import style from "./MarketPlace.module.css";
+
 export default function MarketPlace() {
   return (
-    <div className={style.container}>
+    <div>
       <MarketPlaceSection />
     </div>
   );
