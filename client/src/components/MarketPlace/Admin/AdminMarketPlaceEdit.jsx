3d36559ae6d753ed1c48129187a60e3dc8c1d186
Marcos Hernan Parella <marcosparella2000@gmail.com>
2023-03-02 20:48:52 -0300

    Nuevas implementaciones

---
+++ client/src/components/MarketPlace/Admin/AdminMarketPlaceEdit.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+// import style from "AdminMarketPlaceEdit.module.css";
+
+export default function AdminMarketPlaceEdit() {
+  return <div>AdminMarketPlaceEdit</div>;
+}
--- client/src/components/MarketPlace/Admin/AdminMarketPlaceSection.jsx
+++ client/src/components/MarketPlace/Admin/AdminMarketPlaceSection.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import AdminMarketPlaceDeactivate from "./AdminMarketPlaceDeactivate";
-import AdminMarketPlaceInput from "./AdminMarketPlaceInput";
+import AdminMarketPlaceEdit from "./AdminMarketPlaceEdit";
 export default function AdminMarketPlaceSection({
   setProducts,
   productName,
@@ -33,40 +33,11 @@ export default function AdminMarketPlaceSection({
           {...{ setProductDeactivate, productDeactivate }}
         />
       )}
-      {/* <AdminMarketPlaceInput
-        name="title"
-        placeholder="Ingrese el nombre del producto"
-        setState={setProductName}
-        value={productDeactivate && productDeactivate.title}
-      />
-      <AdminMarketPlaceInput
-        name="price"
-        placeholder="Ingrese el titulo del producto"
-        setState={setProductPrice}
-        value={productDeactivate && productDeactivate.price}
-      />
-      <AdminMarketPlaceInput
-        name="category"
-        placeholder="Ingrese la categoría del producto"
-        setState={setProductCategory}
-        value={productDeactivate && productDeactivate.category}
-      />
-      <AdminMarketPlaceInput
-        name="detail"
-        placeholder="Ingrese la descripción del producto"
-        setState={setProductDetail}
-        value={productDeactivate && productDeactivate.detail}
-      />
-      <AdminMarketPlaceInput
-        name="mainImage"
-        placeholder="Ingrese la imagen principal del producto"
-        setState={setProductMainImage}
-      />
-      <AdminMarketPlaceInput
-        name="images"
-        placeholder="Ingrese las imágenes del producto"
-        setState={setProductImages}
-      /> */}
+      {buttonOption === "edit" && (
+        <AdminMarketPlaceEdit
+          {...{ setProductDeactivate, productDeactivate }}
+        />
+      )}
     </div>
   );
 }
