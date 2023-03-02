8c1bbb1949f637919e241491831649b4a5706d16
MatiasxVarela <saxtebg@gmail.com>
2023-03-02 15:22:09 -0300

    Remake form

--- client/src/components/SignUp/FormSignUp.jsx
+++ client/src/components/SignUp/FormSignUp.jsx
@@ -1,4 +1,6 @@
-import { ErrorMessage, Field, Form, Formik } from "formik";
+import { useFormik } from "formik";
+import { Form, Input } from "semantic-ui-react";
+
 import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { useEffect } from "react";
@@ -49,6 +51,72 @@ export default function FormSignUp() {
     return toDay;
   };
 
+  const initialValues = {
+    genre: "",
+    email: "",
+    nickName: "",
+    //imagen default
+    image: "https://i.ibb.co/86tPY9X/PF-profile-01.png",
+    firstName: "",
+    lastName: "",
+    dateOfBirth: "",
+    password: "",
+    repeatPassword: "",
+  };
+  
+  const validationSchema = Yup.object({
+    genre: Yup.string().required("El género es obligatorio"),
+    email: Yup.string()
+      .email("Email no válido")
+      .matches(/^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/, "Email no válido")
+      .required("El email es obligatorio"),
+    nickName: Yup.string()
+      .min(3, "Debe tener más de 3 caracteres")
+      .max(15, "Debe tener menos de 15 caracteres")
+      .required("El nombre de usuario es obligatorio"),
+    firstName: Yup.string()
+      .min(3, "Debe tener más de 3 caracteres")
+      .max(55, "Debe tener menos de 55 caracteres")
+      .required("El nombre es obligatorio"),
+    lastName: Yup.string()
+      .min(3, "Debe tener más de 3 caracteres")
+      .max(55, "Debe tener menos de 55 caracteres")
+      .required("El apellido es obligatorio"),
+    dateOfBirth: Yup.string().required(
+      "La fecha de nacimiento es obligatoria"
+    ),
+    password: Yup.string()
+      .min(8, "Debe tener más de 8 caracteres")
+      .required("La contraseña es obligatoria"),
+    repeatPassword: Yup.string()
+      .oneOf(
+        [Yup.ref("password")],
+        "Las contraseñas deben ser iguales"
+      )
+      .required("Debe repetir la contraseña"),
+  });
+  
+  const onSubmit = (values) => {
+    const obj = {
+      genre: values.genre,
+      email: values.email,
+      nickName: values.nickName,
+      image: values.image,
+      firstName: values.firstName,
+      lastName: values.lastName,
+      dateOfBirth: values.dateOfBirth,
+      password: values.password,
+    };
+    dispatch(postUser(obj));
+    navigate("/logIn");
+  };
+  
+  const formik = useFormik({
+    initialValues,
+    validationSchema,
+    onSubmit,
+  });
+  
   return (
     <div className={style.landing}>
       <img
@@ -59,218 +127,166 @@ export default function FormSignUp() {
       <div className={style.container}>
         <div className={style.formContainer}>
           <h1 className={style.formTitle}>REGISTRARSE</h1>
-          <Formik
-            initialValues={{
-              genre: "",
-              email: "",
-              nickName: "",
-              //imagen default
-              image: "https://i.ibb.co/86tPY9X/PF-profile-01.png",
-              firstName: "",
-              lastName: "",
-              dateOfBirth: "",
-              password: "",
-              repeatPassword: "",
-              //nationality: "",
-            }}
-            validationSchema={Yup.object({
-              genre: Yup.string().required("El género es obligatorio"),
-              email: Yup.string()
-                .email("Email no válido")
-                .matches(/^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/, "Email no válido")
-                .required("El email es obligatorio"),
-              nickName: Yup.string()
-                .min(3, "Debe tener más de 3 caracteres")
-                .max(15, "Debe tener menos de 15 caracteres")
-                .required("El nombre de usuario es obligatorio"),
-              firstName: Yup.string()
-                .min(3, "Debe tener más de 3 caracteres")
-                .max(55, "Debe tener menos de 55 caracteres")
-                .required("El nombre es obligatorio"),
-              lastName: Yup.string()
-                .min(3, "Debe tener más de 3 caracteres")
-                .max(55, "Debe tener menos de 55 caracteres")
-                .required("El apellido es obligatorio"),
-              dateOfBirth: Yup.string().required(
-                "La fecha de nacimiento es obligatoria"
-              ),
-              password: Yup.string()
-                .min(8, "Debe tener más de 8 caracteres")
-                .required("La contraseña es obligatoria"),
-              repeatPassword: Yup.string()
-                .oneOf(
-                  [Yup.ref("password")],
-                  "Las contraseñas deben ser iguales"
-                )
-                .required("Debe repetir la contraseña"),
-            })}
-            onSubmit={(values) => {
-              const obj = {
-                genre: values.genre,
-                email: values.email,
-                nickName: values.nickName,
-                image: values.image,
-                firstName: values.firstName,
-                lastName: values.lastName,
-                dateOfBirth: values.dateOfBirth,
-                password: values.password,
-              };
-              dispatch(postUser(obj));
-              navigate("/logIn");
-            }}
-          >
-            <Form>
-              <div className={style.formSubCont}>
-                {/* ------------------------------------------------------------------------- */}
-                <div className={style.formInputs}>
-                  <label
-                    htmlFor="firstName"
-                    className={style.inputTitle}
-                  >
-                    Nombre
-                  </label>
-                  <Field
-                    name="firstName"
-                    type="text"
-                  />
-                  <ErrorMessage name="firstName" />
-                </div>
-                {/* ------------------------------------------------------------------------- */}
-                <div className={style.formInputs}>
-                  <label
-                    htmlFor="lastName"
-                    className={style.inputTitle}
-                  >
-                    Apellido
-                  </label>
-                  <Field
-                    name="lastName"
-                    type="text"
-                  />
-                  <ErrorMessage name="lastName" />
-                </div>
-                {/* ------------------------------------------------------------------------- */}
-                <div className={style.formInputs}>
-                  <label
-                    htmlFor="nickName"
-                    className={style.inputTitle}
-                  >
-                    Nombre de usuario
-                  </label>
-                  <Field
-                    name="nickName"
-                    type="text"
-                  />
-                  <ErrorMessage name="nickName" />
-                </div>
-                {/* ------------------------------------------------------------------------- */}
-                <div className={style.formInputs}>
-                  <label
-                    htmlFor="email"
-                    className={style.inputTitle}
-                  >
-                    Email
-                  </label>
-                  <Field
-                    name="email"
-                    type="email"
-                  />
-                  <ErrorMessage name="email" />
-                </div>
-                {/* ------------------------------------------------------------------------- */}
-                <div className={style.formInputs}>
-                  <label
-                    htmlFor="password"
-                    className={style.inputTitle}
-                  >
-                    Contraseña
-                  </label>
-                  <Field
-                    name="password"
-                    type="password"
-                  />
-                  <ErrorMessage name="password" />
-                </div>
-                {/* ------------------------------------------------------------------------- */}
-                <div className={style.formInputs}>
-                  <label
-                    htmlFor="repeatPassword"
-                    className={style.inputTitle}
-                  >
-                    Repetir contraseña
-                  </label>
-                  <Field
-                    name="repeatPassword"
-                    type="password"
-                  />
-                  <ErrorMessage name="repeatPassword" />
-                </div>
-                {/* ------------------------------------------------------------------------- */}
-                <div className={style.formInputs}>
-                  <label
-                    htmlFor="dateOfBirth"
-                    className={style.inputTitle}
-                  >
-                    Fecha de nacimiento
-                  </label>
-                  <Field
-                    name="dateOfBirth"
-                    type="date"
-                    max={getDateActually()}
-                  />
-                  <ErrorMessage name="dateOfBirth" />
-                </div>
-                {/* ------------------------------------------------------------------------- */}
-                <div className={style.formInputs}>
-                  <label
-                    htmlFor="genre"
-                    className={style.inputTitle}
-                  >
-                    Género
-                  </label>
-                  <Field
-                    name="genre"
-                    as="select"
-                  >
-                    <option
-                      value="Mostrar opciones"
-                      hidden={true}
-                    >
-                      --Mostrar opciones--
-                    </option>
-                    <option value="Femenino">Femenino</option>
-                    <option value="Masculino">Masculino</option>
-                    <option value="No binario">No binario</option>
-                    <option value="Prefiero no decirlo">
-                      Prefiero no decirlo
-                    </option>
-                    <option value="Otro">Otro</option>
-                  </Field>
-                  <ErrorMessage name="genre" />
-                </div>
-                {/* ------------------------------------------------------------------------- */}
-                <button
-                  type="submit"
-                  className={style.btnSignup}
+
+          <form onSubmit={formik.handleSubmit}>
+            <div
+              className={style.formInputs}
+              >
+                
+              <label htmlFor="firstName"
+                className={style.inputTitle}
+              >Nombre</label>
+              <Form.Input
+                id="firstName"
+                name="firstName"
+                type="text"
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.firstName}
+                />
+                {formik.touched.firstName && formik.errors.firstName ? (
+                <div>{formik.errors.firstName}</div>
+                ) : null}
+
+              <label htmlFor="lastName"
+                className={style.inputTitle}
+              >Apellido</label>
+              <Form.Input
+                id="lastName"
+                name="lastName"
+                type="text"
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.lastName}
+                />
+                {formik.touched.lastName && formik.errors.lastName ? (
+                <div>{formik.errors.lastName}</div>
+                ) : null}
+
+              <label htmlFor="nickName"
+                className={style.inputTitle}
+              >Nombre de usuario</label>
+              <Form.Input
+                id="nickName"
+                name="nickName"
+                type="text"
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.nickName}
+                />
+                {formik.touched.nickName && formik.errors.nickName ? (
+                <div>{formik.errors.nickName}</div>
+                ) : null}
+
+              <label htmlFor="email"
+                className={style.inputTitle}
+              >Email</label>
+              <Form.Input
+                id="email"
+                name="email"
+                type="text"
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.email}
+                />
+                {formik.touched.email && formik.errors.email ? (
+                <div>{formik.errors.email}</div>
+                ) : null}
+
+              <label htmlFor="password"
+                className={style.inputTitle}
+              >Contraseña</label>
+              <Form.Input
+                id="password"
+                name="password"
+                type="password"
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.password}
+                />
+                {formik.touched.password && formik.errors.password ? (
+                <div>{formik.errors.password}</div>
+                ) : null}
+
+              <label htmlFor="repeatPassword"
+                className={style.inputTitle}
+              >Repite la contraseña</label>
+              <Form.Input
+                id="repeatPassword"
+                name="repeatPassword"
+                type="password"
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.repeatPassword}
+                />
+                {formik.touched.repeatPassword && formik.errors.repeatPassword ? (
+                <div>{formik.errors.repeatPassword}</div>
+                ) : null}
+
+              <label htmlFor="dateOfBirth"
+                className={style.inputTitle}
+              >Fecha de nacimiento</label>
+              <Form.Input
+                id="dateOfBirth"
+                name="dateOfBirth"
+                type="date"
+                max={getDateActually()}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.dateOfBirth}
+                />
+                {formik.touched.dateOfBirth && formik.errors.dateOfBirth ? (
+                <div>{formik.errors.dateOfBirth}</div>
+                ) : null}
+
+                <label 
+                  htmlFor="genre"
+                  className={style.inputTitle}
+                >Género:
+                </label>
+                <select
+                  name="genre"
+                  id="genre"
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.genre}
+                  className="ui dropdown"
                 >
-                  Registrarse
-                </button>
-                <button
-                  onClick={handleSingUpWithGoogle}
-                  className={style.btnGoogle}
-                >
-                  <div className={style.googleCont}>
-                    <img
-                      src={googleLogo}
-                      className={style.googleLogo}
-                      alt="voyHENRY logo"
-                    />
-                    <p>Continuar con Google</p>
-                  </div>
-                </button>
-              </div>
-            </Form>
-          </Formik>
+                  <option value="">--Selecciona una opción--</option>
+                  <option value="Femenino">Femenino</option>
+                  <option value="Masculino">Masculino</option>
+                  <option value="No binario">No binario</option>
+                  <option value="Prefiero no decirlo">Prefiero no decirlo</option>
+                  <option value="Otro">Otro</option>
+                  </select>
+                  {formik.touched.genre && formik.errors.genre ? (
+                  <div>{formik.errors.genre}</div>
+                  ) : null}
+
+                
+              <button 
+                className={style.btnSignup}
+                type="submit"
+              >
+                Enviar
+              </button>
+            </div>
+          </form>
+
+          <button
+            onClick={handleSingUpWithGoogle}
+            className={style.btnGoogle}
+            >
+            <div className={style.googleCont}>
+              <img
+                src={googleLogo}
+                className={style.googleLogo}
+                alt="Imagen de botón para continuar con google"
+                />
+              <p>Continuar con Google</p>
+            </div>
+          </button>
+
           <button
             onClick={() => navigate("/")}
             className={style.btnVolver}
