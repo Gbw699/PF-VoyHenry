795efde125fd9b73532cbc56bfe8d706ad20160b
Marcos Hernan Parella <marcosparella2000@gmail.com>
2023-03-03 20:06:14 -0300

    Creación de productos

--- client/src/components/MarketPlace/Admin/AdminMarketPlaceCreate.jsx
+++ client/src/components/MarketPlace/Admin/AdminMarketPlaceCreate.jsx
@@ -3,11 +3,9 @@ import React from "react";
 
 import { useDispatch, useSelector } from "react-redux";
 import AdminMarketPlaceInput from "./AdminMarketPlaceInput";
-import { editProduct } from "../../../redux/slices/marketPlaceSlice/thunk";
+import { createProduct } from "../../../redux/slices/marketPlaceSlice/thunk";
 
 export default function AdminMarketPlaceCreate({
-  editProducts,
-  setEditProducts,
   productName,
   setProductName,
   productPrice,
@@ -20,60 +18,28 @@ export default function AdminMarketPlaceCreate({
   setProductMainImage,
   productImages,
   setProductImages,
+  productStock,
+  setProductStock,
 }) {
   const dispatch = useDispatch();
-  const { products } = useSelector(
-    (state) => state.marketPlaceStore.filteredProducts
-  );
 
-  const handleProductSelection = ({ target: { value } }) => {
-    const product =
-      value !== "unselected"
-        ? products.find((product) => product.id === parseInt(value))
-        : "";
-    setEditProducts(product);
-    setProductName(product.title);
-    setProductPrice(product.price);
-    setProductCategory(product.category);
-    setProductDetail(product.detail);
-    setProductMainImage(product.mainImage);
-    setProductImages(product.Images);
-  };
-
-  const handleEdit = () => {
-    editProducts
-      ? dispatch(
-          editProduct(editProducts.id, {
-            title: productName,
-            price: productPrice,
-            category: productCategory,
-            detail: productDetail,
-            mainImage: productMainImage,
-            images: productImages,
-          })
-        )
-      : window.alert("No has seleccionado ningún producto para editar");
+  const handleCreate = () => {
+    dispatch(
+      createProduct({
+        title: productName,
+        price: productPrice,
+        category: productCategory,
+        detail: productDetail,
+        mainImage: productMainImage,
+        images: [productImages],
+        stock: productStock,
+      })
+    );
+    // : window.alert("No has seleccionado ningún producto para editar");
   };
 
   return (
     <div>
-      {!products ? (
-        <select>
-          <option>No hay productos</option>
-        </select>
-      ) : (
-        <select onChange={handleProductSelection}>
-          <option value="unselected">Seleccionar un producto</option>
-          {products.map((element) => (
-            <option
-              value={element.id}
-              key={element.id}
-            >
-              {element.title}
-            </option>
-          ))}
-        </select>
-      )}
       <AdminMarketPlaceInput
         name="title"
         placeholder="Ingrese el nombre del producto"
@@ -82,9 +48,15 @@ export default function AdminMarketPlaceCreate({
       />
       <AdminMarketPlaceInput
         name="price"
-        placeholder="Ingrese el titulo del producto"
+        placeholder="Ingrese el precio del producto"
         value={productPrice}
         setState={setProductPrice}
+      />
+      <AdminMarketPlaceInput
+        name="stock"
+        placeholder="Ingrese el stock del producto"
+        value={productStock}
+        setState={setProductStock}
       />
       <AdminMarketPlaceInput
         name="category"
@@ -110,30 +82,8 @@ export default function AdminMarketPlaceCreate({
         value={productImages}
         setState={setProductImages}
       />
-      {editProducts ? (
-        <img
-          src={editProducts.mainImage}
-          alt={editProducts.title}
-          title={editProducts.title}
-        />
-      ) : (
-        <img
-          src=""
-          alt="Sin foto"
-          title="Sin foto"
-        />
-      )}
-      {editProducts &&
-        editProducts.images.map((image, index) => (
-          <div key={index}>
-            <img
-              src={image}
-              alt={editProducts.title}
-              title={editProducts.title}
-            />
-          </div>
-        ))}
-      <button onClick={handleEdit}>Editar</button>
+
+      <button onClick={handleCreate}>Editar</button>
     </div>
   );
 }
