eeb09abf28dbb1f9ad95a48e1dad509dd7414036
MatiasxVarela <saxtebg@gmail.com>
2023-03-03 08:18:23 -0300

    Add socket.io

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 const app = require('./app');
 const sequelize = require('./src/libs/database/database');
+const socketIO = require('socket.io');
+const http = require('http');
 
 /* ➖➖🟥🟥🟥
  ➖🟥🟥🟦🟦🟦
@@ -17,7 +19,15 @@ async function main() {
   try {
     await sequelize.sync({ force: false, logging: false });
     console.log('Connection has been established successfully.');
-    app.listen(PORT, () => {
+
+    const server = http.createServer(app);
+    const io = socketIO(server);
+
+    io.on('connection', (socket) => {
+      console.log('A user has connected');
+    });
+
+    server.listen(PORT, () => {
       console.log(`Server started on port ${PORT}`);
     });
   } catch (error) {
