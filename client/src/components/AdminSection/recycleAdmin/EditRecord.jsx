688ac04655be3e20bcde112225fd92e3010c35d0
Gabriel Barimboim <gdbarimboim@gmail.com>
2023-03-03 18:39:47 -0300

    edir record commit

--- client/src/components/AdminSection/recycleAdmin/EditRecord.jsx
+++ client/src/components/AdminSection/recycleAdmin/EditRecord.jsx
@@ -1,9 +1,3 @@
 export default function EditRecord(argt, location, renderValue, Set) {
-  return (
-    <div>
-      <h3>{argt}</h3>
-      <h3>{location}</h3>
-      <h3>hola</h3>
-    </div>
-  );
+  console.log(argt);
 }
