935c03729e884ba179d029ce4452ef6192640027
Marcos Hernan Parella <marcosparella2000@gmail.com>
2023-03-03 21:33:25 -0300

    Arreglo en editar producto
    Co-Authored-By: Lien! <80282938+liensanchez@users.noreply.github.com>

--- api/src/schemas/products.schema.js
+++ api/src/schemas/products.schema.js
@@ -1,53 +1,55 @@
-const Joi = require('joi')
+const Joi = require('joi');
 
-const id = Joi.number()
-const title = Joi.string().min(3).max(55)
-const price = Joi.number().greater(0)
-const quantity = Joi.number()
-const detail = Joi.string().min(5).max(255)
-const mainImage = Joi.string().uri()
-const availability = Joi.boolean()
-const user = Joi.string()
-const category = Joi.string().valid('Remeras', 'Pantalones',  'Gorros')
-const images = Joi.array()
+const id = Joi.number();
+const title = Joi.string().min(3).max(55);
+const price = Joi.number().greater(0);
+const stock = Joi.number();
+const quantity = Joi.number();
+const detail = Joi.string().min(5).max(255);
+const mainImage = Joi.string().uri();
+const availability = Joi.boolean();
+const user = Joi.string();
+const category = Joi.string().valid('Remeras', 'Pantalones', 'Gorros');
+const images = Joi.array();
 
 const getProductSchema = Joi.object({
-  id: id.required()
-})
+  id: id.required(),
+});
 
 const createProductSchema = Joi.object({
   title: title.required(),
+  stock: stock.required(),
   price: price.required(),
   detail: detail.required(),
   mainImage: mainImage.required(),
   availability: availability.required(),
 
   category: category.required(),
-  images:images.required()
-
-})
+  images: images.required(),
+});
 
 const updateProductSchema = Joi.object({
   title: title,
   price: price,
+  stock: stock,
   detail: detail,
   mainImage: mainImage,
   availability: availability,
   category: category,
-  images:images
-})
+  images: images,
+});
 
 const buyProductSchema = Joi.object({
   id: id,
   title: title,
   price: price,
   quantity: quantity,
-  user: user
-})
+  user: user,
+});
 
 module.exports = {
   getProductSchema,
   createProductSchema,
   updateProductSchema,
-  buyProductSchema
-}
+  buyProductSchema,
+};
