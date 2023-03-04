4f17d871eff0fda732b1bc7db1c69d38c33ed96a
MatiasxVarela <saxtebg@gmail.com>
2023-03-04 10:39:40 -0300

    Auth in sessions routes

--- api/src/routes/sessions.routes.js
+++ api/src/routes/sessions.routes.js
@@ -1,18 +1,23 @@
 const { Router } = require('express');
+const passport = require('passport')
 const router = Router()
+const { checkAdminRole } = require('../middlewares/auth.handler')
 const SessionsService = require('../services/sessions.service')
 
 const sessionService = new SessionsService()
 
-router.get('', async (req, res, next) => {
-  try {
-    const allSessions = await sessionService.find()
+router.get('/',
+  passport.authenticate('jwt', {session: false}),
+  checkAdminRole,
+  async (req, res, next) => {
+    try {
+      const allSessions = await sessionService.find()
 
-    res.json(allSessions)
-  } catch (error) {
+      res.json(allSessions)
+    } catch (error) {
 
-    next(error)
-  }
+      next(error)
+    }
 
 });
 
