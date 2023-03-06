8392e711c64b12ad95c002aa79fe4ba49be9823d
FacuTombesi <facundomtombesi@gmail.com>
2023-03-06 14:25:10 -0300

    CSS blog filters fixes

--- client/src/components/Blog/DateFilter.jsx
+++ client/src/components/Blog/DateFilter.jsx
@@ -1,4 +1,4 @@
-import style from "./DateFilter.module.css";
+import style from "./BlogFilters.module.css";
 
 export default function DateFilter({ filters, setFilters }) {
   return (
@@ -9,7 +9,7 @@ export default function DateFilter({ filters, setFilters }) {
         color="#f1e100"
       />
       <input
-        className={style.input}
+        className={style.inputs}
         type="date"
         name="dateInput"
         onChange={(event) =>
