42bad751b13b5567415d15c1f8d0e882ef22c63d
FacuTombesi <facundomtombesi@gmail.com>
2023-03-07 11:10:39 -0300

    CSS fixes + favorites

--- client/src/components/Favorite/Favorite.jsx
+++ client/src/components/Favorite/Favorite.jsx
@@ -1,8 +1,11 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import PlanCard from "../../recycle/PlanCard/PlanCard";
+import style from "./Favorite.module.css";
 
 export default function Favorite() {
+    const navigate = useNavigate();
     const user = JSON.parse(localStorage.getItem("user"));
     const [favorites, setFavorites] = useState();
 
@@ -16,25 +19,35 @@ export default function Favorite() {
     };
 
     if(!favorites){
-        return <div>Loading...</div>;
+        return <div className={style.container}>Aún no has agregado nada a favoritos</div>;
     }
 
     return (
-        <div>
-            <div>
-                {favorites?.map((plan) => (
-                    <PlanCard
-                        key={plan.id}
-                        id={plan.id}
-                        country={plan.country}
-                        province={plan.province}
-                        mainImage={plan.mainImage}
-                        title={plan.title}
-                        summary={plan.summary}
-                        eventDate={plan.eventDate}
-                        average={plan.average}
-                    />
-                ))}
+        <div className={style.container}>
+            <div className={style.favoriteCont}>
+                <p className={style.title}>Mis favoritos</p>
+                <hr color="#f1e100" width="100%" />
+                <div className={style.plansCont}>
+                    {favorites?.map((plan) => (
+                        <PlanCard
+                            key={plan.id}
+                            id={plan.id}
+                            country={plan.country}
+                            province={plan.province}
+                            mainImage={plan.mainImage}
+                            title={plan.title}
+                            summary={plan.summary}
+                            eventDate={plan.eventDate}
+                            average={plan.average}
+                        />
+                    ))}
+                </div>
+                <button
+                    onClick={() => navigate("/profile")}
+                    className={style.backBtn}
+                >
+                    Volver
+                </button>
             </div>
         </div>
     );
--- client/src/components/Profile/ProfileMyPlans.jsx
+++ client/src/components/Profile/ProfileMyPlans.jsx
@@ -1,19 +1,12 @@
 import React from "react";
-import { useEffect, useState } from "react";
 import style from "./ProfileMyPlans.module.css";
 
 export default function ProfileMyPlans({ myPlansImage, myPlansName }) {
-  const [backgroundImage, setBackgroundImage] = useState("");
-
-  useEffect(() => {
-    setBackgroundImage(`url(${myPlansImage})`);
-  });
-
   return (
     <div className={style.container}>
       <div
         className={style.img}
-        style={{ backgroundImage: backgroundImage }}
+        style={{ backgroundImage: `url(${myPlansImage})` }}
       >
         <hr
           color="white"
