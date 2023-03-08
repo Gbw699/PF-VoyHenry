07d122e0129597fb239a94a31eaf51ced0b093cc
Julian <cococastro619@gmail.com>
2023-03-08 14:03:03 -0300

    Arreglos en nickName

--- client/src/redux/slices/blogSlice/thunk.js
+++ client/src/redux/slices/blogSlice/thunk.js
@@ -29,7 +29,8 @@ export const getBlogById = (id) => {
   return async (dispatch) => {
     try {
       const response = await axios.get(`/api/v1/blogs/${id}`);
-      dispatch(setBlogById(response.data));
+      console.log(response.data.data);
+      dispatch(setBlogById(response.data.data));
     } catch (error) {
       console.error(error.response);
     }
