bfd1f27c6fd4a63436ad34b10075fd51b869d3fd
MatiasxVarela <saxtebg@gmail.com>
2023-03-08 12:17:47 -0300

    Try catch socket io

--- api/src/libs/SocketIo/socket.js
+++ api/src/libs/SocketIo/socket.js
@@ -10,41 +10,54 @@ const startSocketIo = async() =>{
   io.on('connection', (socket) => {
     console.log("Clientes conectados: ", io.engine.clientsCount)
 
-    sessionsService.initSession(
-      socket.handshake.auth.nickName,
-      socket.handshake.auth.completeName,
-      socket.id
-    )
+    try {
+      sessionsService.initSession(
+        socket.handshake.auth.nickName,
+        socket.handshake.auth.completeName,
+        socket.id
+      )
+    } catch (error) {
+      console.error(error);
+    }
 
     socket.on('mensaje', async (data) => {
-      await messagesService.create({
-        to: data.to,
-        from: data.from,
-        message: data.message
-      })
+      try {
+        await messagesService.create({
+          to: data.to,
+          from: data.from,
+          message: data.message
+        })
 
-      let firstUserSession = await sessionsService.findOne(data.to)
-      let secondUserSessions = await sessionsService.findOne(data.from)
+        let firstUserSession = await sessionsService.findOne(data.to)
+        let secondUserSessions = await sessionsService.findOne(data.from)
 
-      if (firstUserSession) {
-        const firstUserSockets = firstUserSession.dataValues.sockets
-        firstUserSockets.forEach(socket => {
-          io.to(socket).emit('newMessage', "Tienes un nuevo mensaje " + socket)
-        });
-      }
+        if (firstUserSession) {
+          const firstUserSockets = firstUserSession.dataValues.sockets
+          firstUserSockets.forEach(socket => {
+            io.to(socket).emit('newMessage', "Tienes un nuevo mensaje " + socket)
+          });
+        }
 
-      if (secondUserSessions) {
-        const secondUserSockets = secondUserSessions.dataValues.sockets
-        secondUserSockets.forEach(socket => {
-          io.to(socket).emit('newMessage', "Tienes un nuevo mensaje " + socket)
-        });
+        if (secondUserSessions) {
+          const secondUserSockets = secondUserSessions.dataValues.sockets
+          secondUserSockets.forEach(socket => {
+            io.to(socket).emit('newMessage', "Tienes un nuevo mensaje " + socket)
+          });
+        }
+      } catch (error) {
+        console.error(error)
       }
 
     })
 
     socket.on('disconnect', () => {
       console.log("Clientes conectados: ", io.engine.clientsCount)
-      sessionsService.finishSession(socket.handshake.auth.nickName, socket.id)
+
+      try {
+        sessionsService.finishSession(socket.handshake.auth.nickName, socket.id)
+      } catch (error) {
+        console.error(error)
+      }
     });
   });
 }
