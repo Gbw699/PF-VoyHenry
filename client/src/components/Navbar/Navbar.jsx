cfa6036141e79f9c730d5294512477ad8f5b7bfb
Marcos Hernan Parella <marcosparella2000@gmail.com>
2023-03-09 10:44:59 -0300

    Número en el carrito del NavBar

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -4,10 +4,24 @@ import { NavLink, useLocation } from "react-router-dom";
 import img from "../../assets/voyHENRY_title.svg";
 import imgCarrito from "../../assets/carrito-de-compras.png";
 import style from "./Navbar.module.css";
+import { useContext } from "react";
+import ProductContext from "../../context/ProductContext";
 
 export default function NavBar() {
   const location = useLocation();
   const user = JSON.parse(localStorage.getItem("user"));
+
+  const productContext = useContext(ProductContext);
+
+  const totalQuantity = () => {
+    const totalesQuantity = productContext.products.map(
+      (product) => product.quantity
+    );
+
+    let total = 0;
+    totalesQuantity.forEach((e) => (total += e));
+    return total;
+  };
 
   const navLinkClassName = (navData) =>
     navData.isActive ? style.linkFontActive : style.linkFont;
@@ -33,8 +47,10 @@ export default function NavBar() {
           <h4>ADMIN</h4>
         </NavLink>
       )}
-      {location.pathname === "/blog" && <SearchBar placeholder="Buscar blog"/>}
-      {location.pathname === "/plans" && <SearchBar placeholder="Buscar plan"/>}
+      {location.pathname === "/blog" && <SearchBar placeholder="Buscar blog" />}
+      {location.pathname === "/plans" && (
+        <SearchBar placeholder="Buscar plan" />
+      )}
       <div className={style.linksCont}>
         <NavLink
           to={"/home"}
@@ -76,6 +92,10 @@ export default function NavBar() {
           to="/marketplace/shoppingcart"
           className={navLinkClassName}
         >
+          <span className="badge badge-sm indicator-item">
+            {totalQuantity()}
+          </span>
+
           <img
             src={imgCarrito}
             width="20px"
