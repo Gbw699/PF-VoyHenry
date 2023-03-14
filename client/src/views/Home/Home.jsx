741eb82b899b85c01bffed8db3657a074a09744a
Marcos Hernan Parella <marcosparella2000@gmail.com>
2023-03-14 18:34:15 -0300

    Se borraron los console.log

--- client/src/views/Home/Home.jsx
+++ client/src/views/Home/Home.jsx
@@ -8,18 +8,16 @@ import useMediaQuery from "@mui/material/useMediaQuery";
 import getMediaQuery from "../../recycle/MediaQuerys/mediaQuerys.mjs";
 
 export default function Home() {
-
   const isMobile = useMediaQuery(getMediaQuery("xs"));
   const isTablet = useMediaQuery(getMediaQuery("sm"));
 
-  console.log(isMobile);
-  console.log(isTablet);
-
   return (
     <div className={style.container}>
-      {!isMobile && !isTablet && <div className={style.profileCont}>
-        <ProfileCard />
-      </div>}
+      {!isMobile && !isTablet && (
+        <div className={style.profileCont}>
+          <ProfileCard />
+        </div>
+      )}
       <div className={style.featured}>
         <h3 className={style.featuredTitle}>Planes Destacados</h3>
         <hr
@@ -27,16 +25,22 @@ export default function Home() {
           color="#F1E100"
         />
         <PlansCardListHome />
-        {!isMobile && <h3 className={style.featuredTitle}>Reseñas Destacadas</h3>}
-        {!isMobile && <hr
-          width="100%"
-          color="#F1E100"
-        />}
+        {!isMobile && (
+          <h3 className={style.featuredTitle}>Reseñas Destacadas</h3>
+        )}
+        {!isMobile && (
+          <hr
+            width="100%"
+            color="#F1E100"
+          />
+        )}
         {!isMobile && <BlogHome />}
       </div>
-      {!isMobile && !isTablet && <div className={style.sections}>
-        <PlansSections />
-      </div>}
+      {!isMobile && !isTablet && (
+        <div className={style.sections}>
+          <PlansSections />
+        </div>
+      )}
     </div>
   );
 }
