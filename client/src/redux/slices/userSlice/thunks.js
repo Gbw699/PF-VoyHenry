d578832ccb86413468cc7d991013f0dd8213be13
MatiasxVarela <matiasxvarela@gmail.com>
2024-01-28 18:11:58 -0300

    try with cookie in promise

--- client/src/redux/slices/userSlice/thunks.js
+++ client/src/redux/slices/userSlice/thunks.js
@@ -30,19 +30,23 @@ export const getLogin = (obj) => {
   };
 };
 
-export const getLoginGoogle = (token, user) => {
-  return async () => {
-      if (token && user){
-        document.cookie =
+export const getLoginGoogle = async (token, user) => {
+  return new Promise((resolve) => {
+    if (token && user) {
+      document.cookie =
         "token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-        document.cookie =
-          "csrftoken=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-        document.cookie = `token=${token}; max-age=604800; path=/;`;
+      document.cookie =
+        "csrftoken=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+      const newCookie = `token=${token}; max-age=604800; path=/;`;
+      document.cookie = newCookie;
 
-        localStorage.setItem("user", JSON.stringify(user));
-      }
-  };
+      localStorage.setItem("user", JSON.stringify(user));
+
+      resolve();
+    }
+  });
 };
+
 
 export const getAllUsers = () => {
   return async (dispatch) => {
