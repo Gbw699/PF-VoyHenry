84eadc93750eed07f6e845ed9211cc2aa42c9dbe
MatiasxVarela <matiasxvarela@gmail.com>
2024-01-29 13:14:38 -0300

    Migrate get Bearer token to js cookie

--- client/src/App.js
+++ client/src/App.js
@@ -26,6 +26,7 @@ import FooterSection from "./components/Footer/FooterSection";
 import AdminSection from "./views/AdminSection/AdminSection";
 import SocketIo from "./views/Socket.io/Socket.io";
 import Messages from "./views/messages/messages";
+import Cookies from "js-cookie";
 
 /* axios.defaults.baseURL = "https://voyhenry.fly.dev/";
 const cookie = document.cookie.split("=");
@@ -34,8 +35,8 @@ const user = JSON.parse(localStorage.getItem("user"));  */
 function App() {
   axios.defaults.baseURL = "https://voyhenry.fly.dev/";
   /* axios.defaults.baseURL = "http://localhost:3001/"; */
-  const cookie = document.cookie.split("=");
-  axios.defaults.headers.common["Authorization"] = `Bearer ${cookie[1]}`;
+  const cookie = Cookies.get("token");
+  axios.defaults.headers.common["Authorization"] = `Bearer ${cookie}`;
   const user = JSON.parse(localStorage.getItem("user"));
   useEffect(() => {}, [document.cookie]);
 
